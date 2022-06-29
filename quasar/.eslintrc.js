d4ae85d59c8d95c2ab17fbc9da327ac67f846296
Jean Claveau <jean.claveau@gmail.com>
2022-06-29 15:33:03 +0200

    Install Storybook with Vite

--- quasar/.eslintrc.js
+++ quasar/.eslintrc.js
@@ -3,42 +3,23 @@ module.exports = {
   // This option interrupts the configuration hierarchy at this file
   // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
   root: true,
-
   parserOptions: {
-    ecmaVersion: '2021', // Allows for the parsing of modern ECMAScript features
-  },
+    ecmaVersion: '2021' // Allows for the parsing of modern ECMAScript features
 
+  },
   env: {
     node: true,
     browser: true,
     'vue/setup-compiler-macros': true
   },
-
   // Rules order is important, please avoid shuffling them
-  extends: [
-    // Base ESLint recommended rules
-    // 'eslint:recommended',
-
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
-    'airbnb-base'
-    
-  ],
-
-  plugins: [
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
-    // required to lint *.vue files
-    'vue',
-    
-  ],
-
+  extends: ['plugin:vue/vue3-essential', 'airbnb-base', 'plugin:storybook/recommended'],
+  plugins: [// https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
+  // required to lint *.vue files
+  'vue'],
   globals: {
-    ga: 'readonly', // Google Analytics
+    ga: 'readonly',
+    // Google Analytics
     cordova: 'readonly',
     __statics: 'readonly',
     __QUASAR_SSR__: 'readonly',
@@ -49,15 +30,12 @@ module.exports = {
     Capacitor: 'readonly',
     chrome: 'readonly'
   },
-
   // add your custom rules here
   rules: {
-    
     'no-param-reassign': 'off',
     'no-void': 'off',
     'no-nested-ternary': 'off',
     'max-classes-per-file': 'off',
-
     'import/first': 'off',
     'import/named': 'error',
     'import/namespace': 'error',
@@ -67,10 +45,8 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
-    
     'prefer-promise-reject-errors': 'off',
-
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+};
