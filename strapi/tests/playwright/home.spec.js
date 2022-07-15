242b6363629f80d101706334b2b8fc743b4b2c63
Jean Claveau <jean.claveau@gmail.com>
2022-07-15 23:00:11 +0200

    Update Home playwright test #20

--- strapi/tests/playwright/home.spec.js
+++ strapi/tests/playwright/home.spec.js
@@ -4,19 +4,28 @@ const { test, expect } = require('@playwright/test');
 test.describe('Home Page', () => {
   test('home api must return basic home content', async ({ request }) => {
 
+    const mainNavResponse = await request.get(`http://localhost:1337/api/navigation/render/main-navigation?type=RFR`);
+    expect(mainNavResponse.ok()).toBeTruthy();
+    let body = await mainNavResponse.json()
 
+    // https://jestjs.io/docs/expect#tomatchobjectobject
+    expect(body.pages.home).toMatchObject({
+      title: expect.stringMatching(/\w+/),
+      path: expect.stringMatching('/'),
+      parent: null,
+      id: 'home',
+      related: {
+        contentType: 'api::page.page',
+        // id: expect.stringMatching(/\d+/),
+      }
+    })
 
-    const homeResponse = await request.get(`http://localhost:1337/api/home`);
+    const homeResponse = await request.get(`http://localhost:1337/api/pages/${body.pages.home.related.id}?populate=deep`);
     expect(homeResponse.ok()).toBeTruthy();
 
-    const body = await homeResponse.json()
+    // body = await homeResponse.json()
+    // console.log(body.data.attributes.sections)
 
-    https://jestjs.io/docs/expect#tomatchobjectobject
-    expect(body.data.attributes.Title).toEqual(expect.stringMatching(/\w+/))
-    expect(body.data.attributes.Description).toEqual(expect.stringMatching(/\w+/))
-    expect(body.data.attributes.Benefits).toEqual(expect.stringMatching(/\w+/))
-    expect(body.data.attributes.slug).toEqual(expect.stringMatching(/^(\w+|-)?$/))
-    expect(body.data.attributes.locale).toEqual("en")
   });
 });
 
