b730bf76ecf55f1a3724d741bd1de3e045b8104b
Jean Claveau <jean.claveau@gmail.com>
2022-07-16 00:34:53 +0200

    Cron job to keep the dyno active Fix #55

--- strapi/config/env/production/server.js
+++ strapi/config/env/production/server.js
@@ -1,7 +1,13 @@
+const cronTasks = require("./cron-tasks");
+
 module.exports = ({ env }) => {
   const serverConfig = {
     port: 1337, // We do not change the port as we are behind a proxy
     proxy: true,
+    cron: {
+      enabled: true,
+      tasks: cronTasks,
+    },
   }
 
   // console.log('env/production/server.js serverConfig', serverConfig)
